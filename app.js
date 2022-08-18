ee049f17058a6e5770c8433c2756d1b4ab96daac
KawshithaFdo <praveenkawshika456@gmail.com>
2022-08-18 13:05:29 +0530

    Add Order.js

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const app=express();
 const port=4000;
 
 
- const customer=require('./routes/customer')
-/*const item=require('./routes/item')
+const customer=require('./routes/customer')
+const item=require('./routes/item')
 const order=require('./routes/order')
-const order_detail=require('./routes/order_detail')*/
+/*const order_detail=require('./routes/order_detail')*/
 
 app.use(express.json());
 app.use('/customer',customer);
-/*app.use('/item',item);
+app.use('/item',item);
 app.use('/order',order);
-app.use('/order_detail',order_detail);*/
+/*app.use('/order_detail',order_detail);*/
 
 app.get('/',(req,res) => {
     res.send("Hello There,This is Express");
