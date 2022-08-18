e6460aa110c902d9f2acd2994a36aa1d14bb1024
KawshithaFdo <praveenkawshika456@gmail.com>
2022-08-18 11:16:27 +0530

    Add mysql configs

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ const express=require('express');
 const app=express();
 const port=4000;
 
+
+const customer=require('./routes/customer')
+const item=require('./routes/item')
+const order=require('./routes/order')
+const order_detail=require('./routes/order_detail')
+
+app.use(express.json);
+app.use('/customer',customer);
+app.use('/item',item);
+app.use('/order',order);
+app.use('/order_detail',order_detail);
+
 app.get('/',(req,res) => {
-    res.send("Hello World");
+    res.send("Hello There,This is Express");
 })
 
 app.listen(port,(req,res)=>{
